b0f52e3a7f475088ae6754cb08cbc5daccdf9ba6
Ankit <contactankit1234@gmail.com>
2025-04-19 18:39:09 +0530

    Add Services Offerings

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,11 +1,13 @@
 // src/pages/Services/Services.jsx
 import React from 'react';
 import ServicesHero from './ServicesHero';
+import ServicesOfferings from './ServicesOfferings';
 
 const Services = () => {
   return (
     <div className="bg-white">
       <ServicesHero/>
+      <ServicesOfferings/>
     </div>
   );
 }
